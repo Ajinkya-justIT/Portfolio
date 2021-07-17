05949aff796217876d055fd659c2fd203efe14ff
Taeon183 <79680787+Taeon183@users.noreply.github.com>
2021-07-17 19:43:44 +0530

    Delete scriptport.js

--- scriptport.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
-            $('.Top').addClass("sticky");
-        }else{
-            $('.Top').removeClass("sticky");
-        }
-    })
-});
